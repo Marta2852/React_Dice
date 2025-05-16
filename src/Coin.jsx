c41ae83eff153de07aadd263b4c45c2859e8beaa
ipa23.m.dzerve <ipa23.m.dzerve@pjvt.local>
2025-05-16 10:50:38 +0300

    Heads or tails start

---
+++ src/Coin.jsx
@@ -0,0 +1,11 @@
+import { useState} from "react";
+import heads from "./assets/coin/heads.png";
+import tails from "./assets/coin/tails.png";
+
+const headTail = [heads, tails];
+
+
+
+function coin(){
+
+}
